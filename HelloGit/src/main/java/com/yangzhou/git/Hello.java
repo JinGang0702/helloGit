1fc5d267462b6d939ac75e04c36f8b8a86ecf0ac
ljg <982465805@qq.com>
2020-11-21 10:40:55 +0800

    创建分支1

--- HelloGit/src/main/java/com/yangzhou/git/Hello.java
+++ HelloGit/src/main/java/com/yangzhou/git/Hello.java
@@ -10,5 +10,6 @@ package com.yangzhou.git;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("创建分支!");
     }
 }
