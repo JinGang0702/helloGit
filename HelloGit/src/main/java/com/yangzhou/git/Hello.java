f4c8e6dabbb0426f0e0dd78499658bd3ab453962
ljg <982465805@qq.com>
2020-11-21 10:01:02 +0800

    提交文件

---
+++ HelloGit/src/main/java/com/yangzhou/git/Hello.java
@@ -0,0 +1,14 @@
+package com.yangzhou.git;
+
+/**
+ * @program: git_workspace
+ * @ClassName: Hello
+ * @description:
+ * @author: ljg
+ * @create: 2020-11-21 09:42
+ **/
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
